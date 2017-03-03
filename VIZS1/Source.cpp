2d44636439ce17f8d4623b13eac2dbb6922f5373
Lukáš Stehlík <brain1231@gmail.com>
2017-03-03 17:42:24 +0100

    Detekcia kruhu

--- VIZS1/Source.cpp
+++ VIZS1/Source.cpp
@@ -12,28 +12,71 @@
 using namespace cv;
 using namespace std;
 
-void help()
-{
-	cout << "\nThis program demonstrates line finding with the Hough transform.\n"
-		"Usage:\n"
-		"./houghlines <image_name>, Default is pic1.jpg\n" << endl;
-}
-
 int main(int argc, char** argv)
 {
-	const char* filename = argc >= 2 ? argv[1] : "pic1.jpg";
-
-	Mat src = imread(filename, 0);
+	Mat src = imread("../Data/kruh2.bmp",IMREAD_UNCHANGED);
 	if (src.empty())
 	{
-		help();
-		cout << "can not open " << filename << endl;
+		cout << "can not open ";
 		return -1;
 	}
 
-	Mat dst, cdst;
-	Canny(src, dst, 50, 200, 3);
-	cvtColor(dst, cdst, CV_GRAY2BGR);
+	Mat dst, HoughSource, HSV_Image;
+	GaussianBlur(src, dst, Size(7, 7), 0, 0); //Rozmazáva hrany
+	medianBlur(dst, dst, 29);
+	cvtColor(dst, HSV_Image, CV_RGB2HSV);
+
+	//imshow("Original", src);
+	//imshow("Gauss",dst);
+	//imshow("Gaus+Median", dst);
+	//Canny(dst, Keny, 50, 200, 3);
+	//imshow("Canny", Keny);
+
+	vector<Vec3f> circles;
+	cvtColor(dst, HoughSource, CV_RGB2GRAY);
+	cout << "Bullshit" << endl;
+	HoughCircles(HoughSource, circles, HOUGH_GRADIENT, 1, HoughSource.rows / 4, 200, 100);
+
+	printf("Pocet kruhov: %u\n", (uint16_t)circles.size());
+
+
+	for (size_t i = 0; i < circles.size(); i++)
+	{
+		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+		int radius = cvRound(circles[i][2]);
+		
+		printf("x=%i y=%i\n", center.x, center.y);
+
+		uint meanH = 0;
+		uint meanS = 0;
+		uint meanV = 0;
+		Vec3b hsv;
+
+		for (int j = (center.x - radius / 2); j < (center.x + radius / 2); j++)
+		{
+			hsv = HSV_Image.at<Vec3b>(j, center.y);
+			meanH += hsv.val[0];
+			meanS += hsv.val[1];
+			meanV += hsv.val[2];
+		}
+
+		uint H = meanH / radius;
+		uint S = meanS / radius;
+		uint V = meanV / radius;
+		printf("H=%u S=%u V=%u\n", H * 2, S, V);
+
+		Mat someColor = Mat(1, 1, CV_8UC3, Scalar(H , S, V));
+		cvtColor(someColor, someColor, COLOR_HSV2RGB);
+		Scalar rgb = someColor.at<Vec3b>(0, 0);
+
+		// draw the circle outline
+		circle(src, center, radius, rgb, 3, 8, 0);
+		putText(src, "Kruh", center, FONT_HERSHEY_PLAIN, 2, Scalar(0, 255, 255), 2);
+	}
+	imshow("Circles", src);
+	waitKey(0);
+	return 0;
+	
 
 #if 0
 	vector<Vec2f> lines;
@@ -57,11 +100,11 @@ int main(int argc, char** argv)
 	for (size_t i = 0; i < lines.size(); i++)
 	{
 		Vec4i l = lines[i];
-		line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
+		line(dst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
 	}
 #endif
 	imshow("source", src);
-	imshow("detected lines", cdst);
+	imshow("detected lines", dst);
 
 	waitKey();
 
